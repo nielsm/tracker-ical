d79d806bacdcdc2464d4ed6bd15b76cfa8724210
Niels Meersschaert <nmeersschaert@mac.com>
2011-04-13 02:03:42 -0400

    Updated configure method to combine token & SSL option.
    Renamed methods to be shorter to call & clearer

--- lib/tracker-ical.rb
+++ lib/tracker-ical.rb
@@ -19,9 +19,17 @@ class TrackerIcal
     PivotalTracker::Client.token(username,password)
   end
   
+  #Sets token & use_ssl in a single method call
+  # configure(:token => 'jkfduisj97823974j2kl24899234', :use_ssl => true)
+  def self.configure(config={})
+    config = {:token => '', :use_ssl => false}.merge!(config)
+    self.token=config[:token]
+    self.use_ssl=config[:use_ssl]    
+  end
+  
   #Returns an ics formatted string of all the iterations and releases in the project
   #If a release does not have a deadline, it will not be included in the output
-  def self.create_calendar_for_project_id(project_id)
+  def self.calendar_for_project(project_id)
     project = PivotalTracker::Project.find(project_id)
     releases = project.stories.all(:story_type => 'release')
     calendar = Icalendar::Calendar.new
@@ -40,10 +48,22 @@ class TrackerIcal
   end
 
   #Creates an ics file at the specified filepath containing the iterations and releases with deadlines for the project_id
-  def self.create_ics_file_for_project_id(filepath,project_id)
+  def self.ics_file_for_project(filepath,project_id)
     file = File.new(filepath,"w+")
     file.write(self.create_calendar_for_project_id(project_id))
     file.close
+  end
+  
+  # <b>DEPRECATED:</b> Please use <tt>calendar_for_project</tt> instead
+  def self.create_calendar_for_project_id(project_id)
+    warn "[DEPRECATION] `create_calendar_for_project_id` is deprecated.  Please use `calendar_for_project` instead."
+    self.calendar_for_project(project_id)
+  end
+  
+  # <b>DEPRECATED:</b> Please use <tt>ics_file_for_project</tt> instead
+  def self.create_ics_file_for_project_id(filepath,project_id)
+    warn "[DEPRECATION] `create_ics_file_for_project_id` is deprecated.  Please use `ics_file_for_project` instead."
+    self.ics_file_for_project(filepath,project_id)
   end
 
   private
